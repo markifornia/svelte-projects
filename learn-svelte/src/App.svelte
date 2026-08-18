66701936ba9150c6c55ca656deb041d835843236
Mark Matanza <mark.matanza@gmail.com>
2026-08-18 15:26:31 -0700

    deeply reactive state

--- learn-svelte/src/App.svelte
+++ learn-svelte/src/App.svelte
@@ -1,7 +1,10 @@
 <script>
-	let count = $state(0)
+	let editor = $state.raw({
+		theme: 'dark',
+		content: '<h1>Svelte</h1>'
+	})
 </script>
 
-<button onclick={() => (count = count + 1)}>
-	{count}
-</button>
+<textarea class="editor" value={editor.content} oninput={e => editor.content = e.target.value} spellcheck="false"></textarea>
+
+{@html editor.content}
