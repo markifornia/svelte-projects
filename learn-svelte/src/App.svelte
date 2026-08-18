eddd3887b3e392b711b28d40797b05b5a01473fe
Mark Matanza <mark.matanza@gmail.com>
2026-08-18 15:40:57 -0700

    destructuring state

--- learn-svelte/src/App.svelte
+++ learn-svelte/src/App.svelte
@@ -1,27 +1,17 @@
 <script>
-	let editor = $state({
+	let { theme, content } = $state({
 		theme: 'dark',
 		content: '<h1>Svelte</h1>'
 	})
-
-	function saveEditorState() {
-		const editorState = structuredClone($state.snapshot(editor))
-		console.log(editorState);
-	}
-
-	saveEditorState();
 </script>
 
 <textarea
 	class="editor"
-	value={editor.content}
+	value={content}
 	oninput={(e) => {
-		editor = {
-			...editor,
-			content: e.target.value
-		}
+		content = e.target.value
 	}}
 	spellcheck="false"
 ></textarea>
 
-{@html editor.content}
+{@html content}
